661489fccc274f73f18118de2b43a464375d6c54
japsancha <japsancha@gmail.com>
2023-06-29 12:28:36 -0100

    add new function 'diminuirValor'

--- src/components/UseState01.jsx
+++ src/components/UseState01.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 export default () => {
 
   const [valor, setValor] = useState(0)
+  const val = 5
 
   function diminuir() {
     setValor(v => v - 1)
@@ -12,10 +13,15 @@ export default () => {
     setValor(v => v + 1)
   }
 
+  function diminuirValor(val) {
+    setValor(v => v - val)
+  }
+
   return (
     <>
       <h4>Use State</h4>
       <p>Valor: {valor}</p>
+      <button onClick={() => diminuirValor(val)}>Diminuir Valor {val}</button>
       <button onClick={diminuir}>Diminuir</button>
       <button onClick={aumentar}>Aumentar</button>
     </>
